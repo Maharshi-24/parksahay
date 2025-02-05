042c7546d2f4dd0f5ca2fa2fc5417723fc0f0f41
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-02-05 16:47:47 +0530

    Updated main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // Import ScreenUtil
 import 'auth/auth_gate.dart';
 
 void main() async {
@@ -13,14 +14,17 @@ class ParkSahayApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Park Sahay',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        fontFamily: 'Roboto',
+    return ScreenUtilInit(
+      designSize: const Size(360, 690), // Base design size for responsiveness
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Park Sahay',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          fontFamily: 'Roboto',
+        ),
+        home: const AuthGate(),
       ),
-      home: const AuthGate(),
     );
   }
 }
