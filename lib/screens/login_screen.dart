342204f364001c71913c0362be6884618b62f11f
Maharshi Desai <133489200+Maharshi-24@users.noreply.github.com>
2025-02-05 17:37:22 +0530

    The auto uncheck of the text field when tapped else where.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,6 +20,9 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _passwordController = TextEditingController();
   String errorMessage = '';
 
+  // Create a FocusNode for managing the focus
+  final FocusNode _focusNode = FocusNode();
+
   Future<void> _signIn() async {
     setState(() => isButtonPressed = true);
     await Future.delayed(Duration(milliseconds: 300)); // Button press delay animation
@@ -46,211 +49,218 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Background - Brutalist Style with More Depth Effect
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.black, Colors.grey.shade900],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+      body: GestureDetector(
+        onTap: () {
+          // Remove the focus when tapping outside of text fields
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Stack(
+          children: [
+            // Background - Brutalist Style with More Depth Effect
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black, Colors.grey.shade900],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
               ),
             ),
-          ),
 
-          // Content with Interactive Animations
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Title with Depth Effect
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 40.sp,
-                    fontWeight: FontWeight.w900,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        color: Colors.grey.shade800,
-                        offset: Offset(6, 6),
-                        blurRadius: 10,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 8.h),
-                Container(
-                  width: 50.w,
-                  height: 4.h,
-                  color: Colors.white,
-                ),
-                SizedBox(height: 32.h),
-
-                // Username Field with Depth Effect and Interaction
-                FocusScope(
-                  child: Focus(
-                    onFocusChange: (focused) {
-                      setState(() => isUsernameFocused = focused);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: isUsernameFocused ? Colors.blue : Colors.white,
-                          width: 3.w,
-                        ),
-                        boxShadow: isUsernameFocused
-                            ? [
-                          BoxShadow(
-                            color: Colors.blue.withOpacity(0.5),
-                            offset: Offset(4, 4),
-                            blurRadius: 8,
-                          ),
-                        ]
-                            : [],
-                        color: Colors.grey.shade900,
-                      ),
-                      child: TextField(
-                        controller: _emailController,
-                        decoration: InputDecoration(
-                          labelText: 'EMAIL',
-                          labelStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(12.w),
-                        ),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16.h),
-
-                // Password Field with Depth Effect and Interaction
-                FocusScope(
-                  child: Focus(
-                    onFocusChange: (focused) {
-                      setState(() => isPasswordFocused = focused);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: isPasswordFocused ? Colors.red : Colors.white,
-                          width: 3.w,
-                        ),
-                        boxShadow: isPasswordFocused
-                            ? [
-                          BoxShadow(
-                            color: Colors.red.withOpacity(0.5),
-                            offset: Offset(4, 4),
-                            blurRadius: 8,
-                          ),
-                        ]
-                            : [],
-                        color: Colors.grey.shade900,
-                      ),
-                      child: TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'PASSWORD',
-                          labelStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.all(12.w),
-                        ),
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 32.h),
-                if (errorMessage.isNotEmpty)
+            // Content with Interactive Animations
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title with Depth Effect
                   Text(
-                    errorMessage,
-                    style: TextStyle(color: Colors.red, fontSize: 14.sp),
-                  ),
-                SizedBox(height: 16.h),
-
-                // Login Button with Sharp Borders and 3D Box Look
-                GestureDetector(
-                  onTap: _signIn,
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 150),
-                    height: isButtonPressed ? 45.h : 50.h,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontSize: 40.sp,
+                      fontWeight: FontWeight.w900,
                       color: Colors.white,
-                      border: Border.all(
-                        color: Colors.black.withOpacity(0.6),
-                        width: 2.w,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.4),
-                          offset: isButtonPressed ? Offset(2, 2) : Offset(4, 4),
-                          blurRadius: isButtonPressed ? 4 : 8,
+                      shadows: [
+                        Shadow(
+                          color: Colors.grey.shade800,
+                          offset: Offset(6, 6),
+                          blurRadius: 10,
                         ),
                       ],
                     ),
-                    child: Center(
-                      child: Text(
-                        'LOGIN',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
-                    ),
                   ),
-                ),
-                SizedBox(height: 16.h),
+                  SizedBox(height: 8.h),
+                  Container(
+                    width: 50.w,
+                    height: 4.h,
+                    color: Colors.white,
+                  ),
+                  SizedBox(height: 32.h),
 
-                // Signup Redirect Text
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignupScreen(),
+                  // Username Field with Depth Effect and Interaction
+                  FocusScope(
+                    child: Focus(
+                      onFocusChange: (focused) {
+                        setState(() => isUsernameFocused = focused);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: isUsernameFocused ? Colors.blue : Colors.white,
+                            width: 3.w,
+                          ),
+                          boxShadow: isUsernameFocused
+                              ? [
+                            BoxShadow(
+                              color: Colors.blue.withOpacity(0.5),
+                              offset: Offset(4, 4),
+                              blurRadius: 8,
+                            ),
+                          ]
+                              : [],
+                          color: Colors.grey.shade900,
                         ),
-                      );
-                    },
-                    child: Text(
-                      "Haven't logged in yet? Press here to sign up.",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.bold,
+                        child: TextField(
+                          controller: _emailController,
+                          focusNode: _focusNode, // Assign the FocusNode here
+                          decoration: InputDecoration(
+                            labelText: 'EMAIL',
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(12.w),
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 16.h),
+
+                  // Password Field with Depth Effect and Interaction
+                  FocusScope(
+                    child: Focus(
+                      onFocusChange: (focused) {
+                        setState(() => isPasswordFocused = focused);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: isPasswordFocused ? Colors.red : Colors.white,
+                            width: 3.w,
+                          ),
+                          boxShadow: isPasswordFocused
+                              ? [
+                            BoxShadow(
+                              color: Colors.red.withOpacity(0.5),
+                              offset: Offset(4, 4),
+                              blurRadius: 8,
+                            ),
+                          ]
+                              : [],
+                          color: Colors.grey.shade900,
+                        ),
+                        child: TextField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'PASSWORD',
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(12.w),
+                          ),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 32.h),
+                  if (errorMessage.isNotEmpty)
+                    Text(
+                      errorMessage,
+                      style: TextStyle(color: Colors.red, fontSize: 14.sp),
+                    ),
+                  SizedBox(height: 16.h),
+
+                  // Login Button with Sharp Borders and 3D Box Look
+                  GestureDetector(
+                    onTap: _signIn,
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 150),
+                      height: isButtonPressed ? 45.h : 50.h,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black.withOpacity(0.6),
+                          width: 2.w,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            offset: isButtonPressed ? Offset(2, 2) : Offset(4, 4),
+                            blurRadius: isButtonPressed ? 4 : 8,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.h),
+
+                  // Signup Redirect Text
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignupScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Haven't logged in yet? Press here to sign up.",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
